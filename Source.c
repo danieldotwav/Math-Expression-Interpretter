7a60dbbbabc8b0ce5c8376f4526bd6fafdceac39
Daniel Rivas <drivas8891@gmail.com>
2024-02-27 09:44:24 -0800

    Add project files.

---
+++ Source.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS /* Disable security warnings in Microsoft Visual Studio */
+
+#include <stdio.h> 
+#include <math.h>
+
+int performAddition(int left_operand, int right_operand);
+int performSubtraction(int left_operand, int right_operand);
+int performMultiplication(int left_operand, int right_operand);
+float performDivision(int left_operand, int right_operand);
+int performModulos(int left_operand, int right_operand);
+
+int main(void) {
+
+	return 0;
+}
+
+int performAddition(int left_operand, int right_operand) {}
+int performSubtraction(int left_operand, int right_operand) {}
+int performMultiplication(int left_operand, int right_operand) {}
+float performDivision(int left_operand, int right_operand) {}
+int performModulos(int left_operand, int right_operand) {}
